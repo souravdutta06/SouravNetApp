b2905745bdb190afeccd1c1a1ff73ab2382c9eb4
souravdutta06 <souravdutta06@gmail.com>
2025-07-20 05:47:14 +0530

    Update Program.cs

--- src/SouravNetApp/Program.cs
+++ src/SouravNetApp/Program.cs
@@ -6,5 +6,12 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Hello Sourav");
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add this to explicitly bind to all interfaces
+        builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+        var app = builder.Build();
     }
+    
 }
